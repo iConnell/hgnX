9dc29d8762fa43c6fcd8c4f23a6b37662105e844
Ikechukwu Ugwuanyi <connell@McConnell.local>
2025-01-30 19:41:41 +0100

    chore: modify for hng 2024

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_credentials=True,
+)
 
 
 @app.get("/api")
-def student_info(track: str = "Backend", slack_name: str = "McConnell Ikechukwu"):
-    data = {
-        "slack_name": slack_name,
-        "current_day": datetime.today().strftime("%A"),
-        "utc_time": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "track": track,
-        "github_file_url": "https://github.com/iConnell/hgnX/blob/master/main.py",
-        "github_repo_url": "https://github.com/iConnell/hgnX",
+def student_info():
+    response = {
+        "data": {
+            "email": "ik.ugwuanyi@gmail.com",
+            "utc_time": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "github_repo_url": "https://github.com/iConnell/hgnX",
+        },
         "status_code": 200,
     }
-    
-    return data
+
+    return response
