e874f08a0e5b6fa185e8c60377f4fed64d6b3db0
Ikechukwu Ugwuanyi <ik.ugwuanyi@gmail.com>
2025-08-05 20:36:06 +0100

    chore: explicitly return plain text response

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import PlainTextResponse
 from typing import Optional
 import os
 from datetime import datetime
 
 app = FastAPI()
 
+
 verify_token = os.environ.get("VERIFY_TOKEN")
 
-
-@app.get("/")
-async def verify_webhook(
-    request: Request
-):
-
+@app.get("/", response_class=PlainTextResponse)
+async def verify_webhook(request: Request):
+    # Extract query parameters with dotted names
     mode = request.query_params.get("hub.mode")
     challenge = request.query_params.get("hub.challenge")
     token = request.query_params.get("hub.verify_token")
 
-    if mode == "subscribe" and verify_token == token:
+    if mode == "subscribe" and token == verify_token:
         print("WEBHOOK VERIFIED")
-        return challenge
+        return PlainTextResponse(content=challenge, status_code=200)
     else:
         raise HTTPException(status_code=403)
-
 
 @app.post("/")
 async def receive_webhook(request: Request):
